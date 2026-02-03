68831a5f5455cdf17bdbc0c12b9e5aaa35bb26d4
IKKIson <shout2517@naver.com>
2026-02-04 01:43:30 +0900

    update CardViewList = Large grid size 3 -> 5

--- src/components/CardViewList.tsx
+++ src/components/CardViewList.tsx
@@ -97,7 +97,7 @@ export const CardViewList = ({
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
       {filteredMaps.map((map) => (
         <CardView key={map.code} {...map} />
       ))}
